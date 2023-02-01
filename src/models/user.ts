4722ed1dea422a8764555944c18d9abf87ae1953
edwinmambo <edwinmambo33@gmail.com>
2023-02-02 02:06:21 +0300

    feat: Updated user model and corresponding tests
    - Updated user model to solve api errors
    - Updated user routes to cater for the update of the api endpoint
    - Updated tests to reflect user model changes

--- src/models/user.ts
+++ src/models/user.ts
@@ -5,20 +5,23 @@ const pepper = process.env.BCRYPT_PASSWORD;
 const saltRounds = process.env.SALT_ROUNDS;
 
 export type User = {
-  id?: number | string;
+  id?: number;
   username: string;
-  password: string;
+  first_name?: string;
+  last_name?: string;
+  password_digest: string;
 };
 
 export class UserStore {
   // get all users
-  async index(): Promise<User[]> {
+  async getAllUsers(): Promise<User[]> {
     try {
+      const sql = 'SELECT * FROM users';
       const conn = await client.connect();
-      const sql = 'SELECT * FROM users RETURNING *';
 
       const result = await conn.query(sql);
       conn.release();
+
       return result.rows;
     } catch (err) {
       throw new Error(`Could not get users. Error ${err}`);
@@ -26,12 +29,14 @@ export class UserStore {
   }
 
   // get one user
-  async show(id: string): Promise<User> {
+  async getUserById(id: number): Promise<User> {
     try {
-      const sql = 'SELECT * FROM users WHERE id=$1 RETURNING *';
+      const sql = 'SELECT * FROM users WHERE id=$1';
       const conn = await client.connect();
-      const result = await conn.query(sql);
+
+      const result = await conn.query(sql, [id]);
       conn.release();
+
       return result.rows[0];
     } catch (err) {
       throw new Error(`Could not find user ${id}. Error: ${err}`);
@@ -39,31 +44,64 @@ export class UserStore {
   }
 
   // create a new user
-  async create(u: User): Promise<User> {
+  async createUser(u: User): Promise<User> {
     try {
       const sql =
-        'INSERT INTO users (username, password_digest) VALUES ($1, $2) RETURNING *';
+        'INSERT INTO users (username, first_name, last_name, password_digest) VALUES ($1, $2, $3, $4) RETURNING *';
       const conn = await client.connect();
 
       const hash = bcrypt.hashSync(
-        u.password + pepper,
-        parseInt(saltRounds || '')
+        u.password_digest + pepper,
+        parseInt(saltRounds as string)
       );
 
-      const result = await conn.query(sql, [u.username, hash]);
-      const user = result.rows[0];
+      const result = await conn.query(sql, [
+        u.username,
+        u.first_name || '',
+        u.last_name || '',
+        hash,
+      ]);
       conn.release();
-      return user;
+
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Could not add new user ${u.username}. Error ${err}`);
+    }
+  }
+
+  // update a user
+  async updateUser(u: User): Promise<User> {
+    try {
+      const sql =
+        'UPDATE users SET username=$2, first_name=$3, last_name=$4, password_digest=$5 WHERE id=$1 RETURNING *';
+      const conn = await client.connect();
+
+      const hash = bcrypt.hashSync(
+        u.password_digest + pepper,
+        parseInt(saltRounds as string)
+      );
+
+      const result = await conn.query(sql, [
+        u.id,
+        u.username,
+        u.first_name,
+        u.last_name,
+        hash,
+      ]);
+      conn.release();
+
+      return result.rows[0];
     } catch (err) {
       throw new Error(`Could not add new user ${u.username}. Error ${err}`);
     }
   }
 
   // delete a user
-  async delete(id: string): Promise<User> {
+  async deleteUser(id: number): Promise<User> {
     try {
       const sql = 'DELETE FROM users WHERE id=$1 RETURNING *';
       const conn = await client.connect();
+
       const result = await conn.query(sql, [id]);
       const user = result.rows[0];
       conn.release();
@@ -75,8 +113,9 @@ export class UserStore {
 
   // authenticate a user
   async authenticate(username: string, password: string): Promise<User | null> {
-    const conn = await client.connect();
     const sql = 'SELECT password_digest FROM users WHERE username=$1';
+
+    const conn = await client.connect();
     const result = await conn.query(sql, [username]);
 
     console.log(password + pepper);
